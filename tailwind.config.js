ec2116f890972d892677e0f31904f823c5d19870
Milena Hamerski <milena.h9@hotmail.com>
2024-07-02 22:20:10 -0300

    Adicionando projeto inicial do portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        "custom-bg": "#1E1E1E",
+        "custom-text": "#FFF8B8",
+        "custom-hover": "#5697C2",
+      },
+      fontFamily: {
+        "sans-narrow": ["PT Sans Narrow", "sans-serif"],
+      },
+      fontSize: {
+        xxl: "2rem",
+        lg: "1.25rem",
+        base: "1.125rem",
+      },
+      margin: {
+        "-20px": "-20px",
+        "-28px": "-28px",
+      },
+      width: {
+        "full-vh": "100vh",
+      },
+      scrollSnapType: {
+        "y-mandatory": "y mandatory",
+      },
+      scrollSnapAlign: {
+        start: "start",
+      },
+      opacity: {
+        90: "0.9",
+      },
+    },
+  },
+};
