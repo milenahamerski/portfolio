b5f5c7f9b31a13b8f4a629ffde7a5c58be1a6c26
Milena Hamerski <hamerskimilena@gmail.com>
2026-04-18 16:49:19 -0300

    Resolves #5: Corrige posicionamento das imagens

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,132 +1,135 @@
 <template>
-  <section id="about" class="w-full h-screen relative snap-start m-0 p-0">
-    <div class="img-fundo absolute top-0 left-24">
-      <img
-        src="/src/assets/images/fundopg2.png"
-        alt="Imagem de Fundo"
-        class="w-64 lg:w-[900px]"
-      />
-    </div>
-    <div class="absolute top-0 right-0 pr-4 ml-5">
-      <img
-        src="/src/assets/images/binariospg2.png"
-        alt="binarios"
-        class="w-64 lg:w-80"
-      />
-    </div>
-    <div class="absolute top-0 right-0 px-6 py-16 mx-4 my-24">
-      <img src="/src/assets/images/pc.png" alt="Logo PC" style="width: 400px" />
-    </div>
-
-    <div class="p-4 md:p-8 lg:p-52 pt-20 h-full flex flex-col justify-between">
-      <div class="flex flex-col flex-grow">
-        <div class="relative mb-6">
-          <div class="absolute inset-y-0 left-0 flex items-center">
-            <div class="w-0.5 h-full bg-custom-hover"></div>
-          </div>
-          <h2 class="relative pl-4 text-xl md:text-xl lg:text-xl">EDUCATION</h2>
-        </div>
-        <div class="text-custom-text text-sm md:text-base lg:text-lg">
-          <p class="font-semibold">
-            Federal University of Technology – Guarapuava, Paraná
-          </p>
-          <p
-            class="text-xs md:text-sm lg:text-base font-light text-custom-gray"
-          >
-            Set 2023 – 2026 (expected): Technologist in Internet Systems,
-            Information Technology
-          </p>
-        </div>
+  <section id="about" class="w-full h-screen relative snap-start m-0 p-0 overflow-hidden">
+    <div class="max-w-screen-2xl mx-auto w-full h-full relative flex flex-col justify-between">
+      <div class="absolute top-0 left-0 sm:left-4 lg:-left-20 z-0 pointer-events-none">
+        <img
+          src="/src/assets/images/fundopg2.png"
+          alt="Imagem de Fundo"
+          class="w-[30rem] lg:w-[900px]"
+        />
       </div>
 
-      <!-- Seção de Experiência de Trabalho -->
-      <div class="flex flex-col flex-grow mt-10 md:mt-16 lg:mt-20 mb-6">
-        <div class="relative mb-6">
-          <div class="absolute inset-y-0 left-0 flex items-center">
-            <div class="w-0.5 h-full bg-custom-hover"></div>
+      <div class="absolute top-0 right-0 pr-4 ml-5 z-0 pointer-events-none">
+        <img
+          src="/src/assets/images/binariospg2.png"
+          alt="binarios"
+          class="w-64 lg:w-80"
+        />
+      </div>
+      <div class="absolute top-0 right-0 px-6 py-16 mx-4 my-24 z-0 pointer-events-none">
+        <img src="/src/assets/images/pc.png" alt="Logo PC" style="width: 400px" />
+      </div>
+
+      <div class="p-4 md:p-8 lg:p-52 pt-20 h-full flex flex-col justify-between relative z-10">
+        <div class="flex flex-col flex-grow">
+          <div class="relative mb-6">
+            <div class="absolute inset-y-0 left-0 flex items-center">
+              <div class="w-0.5 h-full bg-custom-hover"></div>
+            </div>
+            <h2 class="relative pl-4 text-xl md:text-xl lg:text-xl">EDUCATION</h2>
           </div>
-          <h2 class="relative pl-4 text-xl md:text-xl lg:text-lg">
-            WORK EXPERIENCE
-          </h2>
-        </div>
-        <div class="text-custom-text text-sm md:text-base lg:text-lg">
-          <div class="relative">
-            <p class="font-semibold">Developer</p>
+          <div class="text-custom-text text-sm md:text-base lg:text-lg">
+            <p class="font-semibold">
+              Federal University of Technology – Guarapuava, Paraná
+            </p>
             <p
               class="text-xs md:text-sm lg:text-base font-light text-custom-gray"
             >
-              Fiqon Tecnologia Exponencial LTDA - Guarapuava, Paraná
+              Set 2023 – 2026 (expected): Technologist in Internet Systems,
+              Information Technology
             </p>
-            <span
-              class="inline-block bg-custom-hover text-custom-text p-2 rounded-lg text-xs md:text-sm lg:text-sm font-light mt-4"
-            >
-              Jul 2023 – Jul 2024
-            </span>
-          </div>
-          <div class="p-4 md:p-6 lg:p-8 pt-6 md:pt-8 lg:pt-10">
-            <ul
-              class="list-disc list-inside text-xs md:text-sm lg:text-base text-custom-text"
-            >
-              <li>
-                Integration of various systems, such as CRMs, ERPs, databases,
-                and e-commerce platforms, using APIs and web services - to
-                ensure efficient communication between platforms.
-              </li>
-              <li>
-                Skills in creating and maintaining RESTful APIs with Node.js and
-                Express.js, along with studies for developing APIs in ADVPL.
-              </li>
-              <li>
-                Development of responsive and user-friendly interfaces using
-                frameworks like Bootstrap.
-              </li>
-              <li>
-                Proficiency in JavaScript to develop advanced solutions that
-                meet specific client demands.
-              </li>
-              <li>
-                Performing robust testing before deployment to ensure the
-                quality and functionality of developed solutions.
-              </li>
-              <li>
-                Active participation in client meetings, ensuring clear and
-                effective communication.
-              </li>
-            </ul>
           </div>
         </div>
 
-        <div
-          class="text-custom-text text-sm md:text-base lg:text-lg pt-6 md:pt-8 lg:pt-10"
-        >
-          <div class="relative">
-            <p class="font-semibold">Care Assistant</p>
-            <p
-              class="text-xs md:text-sm lg:text-base font-light text-custom-gray"
-            >
-              The Beeches Residential Home - Canterbury, England
-            </p>
-            <span
-              class="inline-block bg-custom-hover text-custom-text p-2 rounded-lg md:text-sm lg:text-sm font-light mt-4"
-            >
-              Mai 2020 – Out 2022
-            </span>
+        <!-- Seção de Experiência de Trabalho -->
+        <div class="flex flex-col flex-grow mt-10 md:mt-16 lg:mt-20 mb-6">
+          <div class="relative mb-6">
+            <div class="absolute inset-y-0 left-0 flex items-center">
+              <div class="w-0.5 h-full bg-custom-hover"></div>
+            </div>
+            <h2 class="relative pl-4 text-xl md:text-xl lg:text-lg">
+              WORK EXPERIENCE
+            </h2>
           </div>
-          <div class="p-4 md:p-6 lg:p-8 pt-6 md:pt-8 lg:pt-10">
-            <ul
-              class="list-disc list-inside text-xs md:text-sm lg:text-base text-custom-text"
-            >
-              <li>
-                Provided personal care and daily assistance for residents.
-              </li>
-              <li>
-                Resolved issues related to residents' well-being and comfort.
-              </li>
-              <li>
-                Improved English through regular communication with residents
-                and colleagues.
-              </li>
-            </ul>
+          <div class="text-custom-text text-sm md:text-base lg:text-lg">
+            <div class="relative">
+              <p class="font-semibold">Developer</p>
+              <p
+                class="text-xs md:text-sm lg:text-base font-light text-custom-gray"
+              >
+                Fiqon Tecnologia Exponencial LTDA - Guarapuava, Paraná
+              </p>
+              <span
+                class="inline-block bg-custom-hover text-custom-text p-2 rounded-lg text-xs md:text-sm lg:text-sm font-light mt-4"
+              >
+                Jul 2023 – Jul 2024
+              </span>
+            </div>
+            <div class="p-4 md:p-6 lg:p-8 pt-6 md:pt-8 lg:pt-10">
+              <ul
+                class="list-disc list-inside text-xs md:text-sm lg:text-base text-custom-text"
+              >
+                <li>
+                  Integration of various systems, such as CRMs, ERPs, databases,
+                  and e-commerce platforms, using APIs and web services - to
+                  ensure efficient communication between platforms.
+                </li>
+                <li>
+                  Skills in creating and maintaining RESTful APIs with Node.js and
+                  Express.js, along with studies for developing APIs in ADVPL.
+                </li>
+                <li>
+                  Development of responsive and user-friendly interfaces using
+                  frameworks like Bootstrap.
+                </li>
+                <li>
+                  Proficiency in JavaScript to develop advanced solutions that
+                  meet specific client demands.
+                </li>
+                <li>
+                  Performing robust testing before deployment to ensure the
+                  quality and functionality of developed solutions.
+                </li>
+                <li>
+                  Active participation in client meetings, ensuring clear and
+                  effective communication.
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div
+            class="text-custom-text text-sm md:text-base lg:text-lg pt-6 md:pt-8 lg:pt-10"
+          >
+            <div class="relative">
+              <p class="font-semibold">Care Assistant</p>
+              <p
+                class="text-xs md:text-sm lg:text-base font-light text-custom-gray"
+              >
+                The Beeches Residential Home - Canterbury, England
+              </p>
+              <span
+                class="inline-block bg-custom-hover text-custom-text p-2 rounded-lg md:text-sm lg:text-sm font-light mt-4"
+              >
+                Mai 2020 – Out 2022
+              </span>
+            </div>
+            <div class="p-4 md:p-6 lg:p-8 pt-6 md:pt-8 lg:pt-10">
+              <ul
+                class="list-disc list-inside text-xs md:text-sm lg:text-base text-custom-text"
+              >
+                <li>
+                  Provided personal care and daily assistance for residents.
+                </li>
+                <li>
+                  Resolved issues related to residents' well-being and comfort.
+                </li>
+                <li>
+                  Improved English through regular communication with residents
+                  and colleagues.
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,69 +1,66 @@
 <template>
-  <div id="home" class="w-full h-screen relative snap-start">
-    <div id="app"></div>
+  <div id="home" class="w-full h-screen relative snap-start overflow-hidden">
+    <div class="max-w-screen-2xl mx-auto w-full h-full relative">
+      <div id="app"></div>
 
-    <div
-      class="relative w-screen h-screen flex items-center justify-center sm:justify-center lg:justify-end"
-    >
       <div
-        class="absolute flex flex-col items-center sm:items-center lg:items-start lg:w-1/3 lg:pl-10"
+        class="relative w-full h-full flex items-center justify-center sm:justify-center lg:justify-end z-10"
       >
         <div
-          id="inicio"
-          class="text-center sm:text-left text-custom-text mt-6 sm:-mt-40 mr-10"
+          class="absolute flex flex-col items-center sm:items-center lg:items-start lg:w-1/3 lg:pl-10"
         >
-          <div class="text-lg sm:text-xxxl lg:text-xxxl">
-            <p class="font-bold">
-              {{ $t("message.hello") }}
-            </p>
-            <p class="font-regular mt-4 text-base sm:text-lg lg:text-lg">
-              {{ $t("message.subtext") }}
-            </p>
-          </div>
           <div
-            class="mt-10 flex justify-center sm:justify-center lg:justify-start w-full"
+            id="inicio"
+            class="text-center sm:text-left text-custom-text mt-6 sm:-mt-40 mr-10"
           >
-            <button @click="goToSocials" class="contact-button">
-              Contact me
-            </button>
+            <div class="text-lg sm:text-xxxl lg:text-xxxl">
+              <p class="font-bold">
+                {{ $t("message.hello") }}
+              </p>
+              <p class="font-regular mt-4 text-base sm:text-lg lg:text-lg">
+                {{ $t("message.subtext") }}
+              </p>
+            </div>
+            <div
+              class="mt-10 flex justify-center sm:justify-center lg:justify-start w-full"
+            >
+              <button @click="goToSocials" class="contact-button">
+                Contact me
+              </button>
+            </div>
           </div>
         </div>
       </div>
+
+      <div id="sobre" class="absolute top-0 left-0 pl-3 ml-3 z-0 pointer-events-none">
+        <img
+          src="/src/assets/images/borboletapg.1.png"
+          alt="Borboleta"
+          class="w-64 lg:w-80"
+        />
+      </div>
+
+      <div id="portfolio-img" class="absolute bottom-0 right-0 pr-4 ml-5 z-0 pointer-events-none">
+        <img
+          src="/src/assets/images/binariospg1.png"
+          alt="binarios"
+          class="w-64 lg:w-80"
+        />
+      </div>
+
+      <div class="composition-group absolute bottom-0 left-0 sm:left-4 lg:-left-20 z-0 pointer-events-none flex items-center justify-center">
+        <img
+          src="/src/assets/images/imgfundopg.1.png"
+          alt="Fundo 1"
+          class="w-[30rem] lg:w-[900px]"
+        />
+        <img
+          src="/src/assets/images/imgMilena.png"
+          alt="Milena"
+          class="absolute z-10 w-48 lg:w-[350px] opacity-95"
+        />
+      </div>
     </div>
-  </div>
-
-  <div id="sobre" class="absolute top-0 left-0 pl-3 ml-3">
-    <img
-      src="/src/assets/images/borboletapg.1.png"
-      alt="Borboleta"
-      class="w-64 lg:w-80"
-    />
-  </div>
-
-  <div
-    class="milena-img absolute bottom-0 left-1/4 pl-16 sm:left-8 sm:pl-4 lg:left-1/4 lg:pl-16"
-  >
-    <img
-      src="/src/assets/images/imgMilena.png"
-      alt="milena"
-      class="w-48 lg:w-72 opacity-95"
-    />
-  </div>
-
-  <div id="portfolio-img" class="absolute bottom-0 right-0 pr-4 ml-5">
-    <img
-      src="/src/assets/images/binariospg1.png"
-      alt="binarios"
-      class="w-64 lg:w-80"
-    />
-  </div>
-
-  <div id="contact" class="contact-img absolute bottom-0 left-20 pl-4">
-    <img
-      src="/src/assets/images/imgfundopg.1.png"
-      alt="Imagem de Fundo"
-      class="w-64 lg:w-[900px]"
-    />
   </div>
 </template>
 
